977e477e19e11ccb8397979bba2d2bdd55e4883e
Abdul Rehman <abdulrehmanali82@gmail.com>
2021-09-15 17:09:42 +0500

    Are you sure prompt

--- public/views/customers.php
+++ public/views/customers.php
@@ -154,3 +154,22 @@
     </div>
   </div>
 </div>
+
+<div class="modal" tabindex="-1" role="dialog" id="gofc-model">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Are You Sure?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn gofc-secondary" data-dismiss="modal">No</button>
+        <button type="button" class="btn gofc-primary confirm-btn">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>
