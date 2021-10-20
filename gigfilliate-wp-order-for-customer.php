42d78d459e7f45eb985b077453915631842670fd
Justin Estrada <jebusiness723@gmail.com>
2021-10-19 23:15:26 -0700

    v1.0.1

--- gigfilliate-wp-order-for-customer.php
+++ gigfilliate-wp-order-for-customer.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              https://partners.vitalibis.com/login
- * @since             1.0.0
+ * @since             1.0.1
  * @package           Gigfilliate_Order_For_Customer
  *
  * @wordpress-plugin
  * Plugin Name:       Gigfilliate Order For Customer
  * Plugin URI:        https://partners.vitalibis.com/login
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Gigfilliate
  * Author URI:        https://partners.vitalibis.com/login
  * License:           GPL-2.0+
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'GIGFILLIATE_ORDER_FOR_CUSTOMER_VERSION', '1.0.0' );
+define( 'GIGFILLIATE_ORDER_FOR_CUSTOMER_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
